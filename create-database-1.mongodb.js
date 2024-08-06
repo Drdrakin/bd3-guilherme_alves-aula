f34ebd3fa3b24cf00d9317d3686e06513f48a3bc
Guilherme Alves <drdrakino@gmail.com>
2024-08-06 20:34:07 -0300

    feat: create database and collection

---
+++ create-database-1.mongodb.js
@@ -0,0 +1,13 @@
+/* Nome do banco de dados */
+const database = 'bd3-guilherme_alves-aula';
+
+/* Nome do banco da coleção de dados */
+const collection = 'livraria';
+
+/* Criar ou acessar o banco de dados */
+use(database);
+
+/* Criar uma coleção de dados */
+db.createCollection(collection);
+
+
